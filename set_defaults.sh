415d9ebfd8fb52a70fda9a75d4857bc89313361c
Sung Min Park <vamf12@gmail.com>
2013-12-01 19:45:23 +0900

    working on new style 	참고 https://trac.cyberduck.io/wiki/help/en/howto/openstack

---
+++ set_defaults.sh
@@ -0,0 +1,2 @@
+defaults write ch.sudo.cyberduck cf.authentication.context '/auth/v1.0'
+defaults write ch.sudo.cyberduck ssproxy.ucloudbiz.olleh.com.certificate.accept 'CN=*.ucloudbiz.olleh.com, OU=Cloud Business Department, O=KT, L=Seongnam-si, S=Gyeonggi-do, C=KR'
